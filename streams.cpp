729094ccd7bd2cdbad47713e07790c1d6e278b49
Sveva Mazzari <smazzari88@gmail.com>
2025-01-27 10:17:26 +0100

    still working on parsing body...

--- streams.cpp
+++ streams.cpp
@@ -1,32 +1,52 @@
-#include <sstream>
+// #include <sstream>
 #include <string>
 #include <iostream>
 
 
 int main(void)
-{
-    std::string str;
-    
-    str =	"HTTP/1.1 404 Not Found\r\n"
-            "Content-Type: text/html\r\n"
-            "Content-Length: 103\r\n" //need to exactly the message's len, or it doesn't work
-            "\r\n"
-            "<html>\r\n"
-            "<header>\r\n"
-            "<title>Not Found</title>\r\n"
-            "</header>\r\n"
-            "<body>\r\n"
-            "<h1>Not Found!!</h1>\r\n"
-            "</body>\r\n"
-            "</html>\r\n";
-    
-    std::istringstream strStream(str);
-    std::string line;
-
-    // strStream >> str;
-
-    while (std::getline(strStream, line))
+{    
+    std::string body = "----WebKitFormBoundary7MA4YWxkTrZu0gW\r\n"
+                       "Content-Disposition: form-data; name=\"field1\"\r\n\r\n"
+                       "value1\r\n"
+                       "----WebKitFormBoundary7MA4YWxkTrZu0gW\r\n"
+                       "Content-Disposition: form-data; name=\"file1\"; filename=\"file.txt\"\r\n"
+                       "Content-Type: text/plain\r\n\r\n"
+                       "This is the content of the file.\r\n"
+                       "----WebKitFormBoundary7MA4YWxkTrZu0gW--";
+    std::string boundary = "----WebKitFormBoundary7MA4YWxkTrZu0gW";
+    std::vector<std::string> myVec;
+    int start = 0;
+    int end = 0;
+    int i = 0;
+    int l = 0;
+    while (i < body.size())
     {
-        std::cout << "line - " << line << std::endl;
+        if (l == 1)
+            break ;
+        if (body.find(boundary, start) != std::string::npos) //go over the boundary
+        {
+            start = i + boundary.size() + 2;
+            // std::cout << "boundary size: " << boundary.size() << "--" << std::endl;
+            // std::cout << body.substr(boundary.size() + 2) << "##" << std::endl;
+            // std::cout << "start index " << start << std::endl;
+            // std::string sub = body.substr(start);
+            // std::cout << sub << "##" << std::endl;
+            if (body.find(boundary, start) != std::string::npos)
+            {
+                end = body.find(boundary, start);
+            }
+            std::string sub = body.substr(start, end - start);
+            std::cout << sub << "##" << std::endl;
+            myVec.push_back(sub);
+            sub.clear();
+        }
+        else
+        {
+            if (body.find(boundary + "--") != std::string::npos)
+                break;
+        }
+        i = end;
+        l++;
     }
+
 }
