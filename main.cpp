00d1556d4192f7d1aff4d137f06bed5c70e36dad
Sveva Mazzari <smazzari88@gmail.com>
2025-02-10 15:05:58 +0100

    rename files with same name

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <sys/socket.h>
+#include <iostream>
+#include <sys/types.h> 
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "Server.hpp"
+#include "InfoServer.hpp"
+
+int main(void)
+{
+	InfoServer		server;	
+	Server 	sockets;
+
+	server.setArrayPorts("8080");
+	server.setArrayPorts("9090");
+	server.setServerRootPath("/home/smazzari/repos/Github/Circle5/webserver/server_root/public/");
+	// server.setServerRootPath("/Users/sveva/repos/Circle5/webserver/server_root/public/"); //test on my laptop
+	sockets.startServer(server);
+	return (0);
+}
