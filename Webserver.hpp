ffd3ac4c747d8bb7108f631b82b03af542bb31a9
Sveva Mazzari <smazzari88@gmail.com>
2025-03-08 21:35:17 +0100

    error

--- Webserver.hpp
+++ Webserver.hpp
@@ -2,11 +2,15 @@
 #define WEBSERVER_H
 
 #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <vector>
 #include <map>
+#include "WebServer.hpp"
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -14,73 +18,52 @@
 #include <sstream>
 #include <stdexcept>
 #include <dirent.h>
-#include <cstring>
+
 #include "ClientRequest.hpp"
 #include "ServerResponse.hpp"
 #include "InfoServer.hpp"
 #include "ServerSockets.hpp"
+#include "StringManipulations.hpp"
 #include "Logger.hpp"
-#include "CGI.hpp"
 
-//#define BUFFER 1024 // moved defs here? (more standard)
-//#define MAX 100
-
-typedef struct ClientTracker {
+typedef struct client
+{
     int fd;
     ClientRequest request;
     std::string response;
-} ClientTracker;
+} client;
 
-typedef struct CGITracker {
-    CGI* cgi;
-    int fd;// Current pipe FD (in_pipe[1] or out_pipe[0])
-    int clientFd;// Added: Original client FD
-    std::string input_data;
-} CGITracker;
+class Webserver
+{
+public:
+                Webserver(InfoServer&);
+                ~Webserver();
+    void	    startServer();
+    void        addServerSocketsToPoll(std::vector<int>);
+    int         fdIsServerSocket(int);
+    void        dispatchEvents();
+    void         createNewClient(int);
+    int         readData(int, std::string&, int&);
+    void        handleReadEvents(int);
+    void        handleWritingEvents(int);
+    ClientRequest        ParsingRequest(std::string, int);
+    void        closeSockets();
+    int        readInChunks(int, std::string&, int&);
+    int         isCgi(std::string);
+    int         searchPage(std::string path);
+    std::string prepareResponse(ClientRequest);
+    std::vector<struct client>::iterator retrieveClient(int fd);
 
-class Webserver {
-    
-    public:
-                    Webserver(InfoServer&);
-                    ~Webserver();
-        void        startServer();
-        void        addServerSocketsToPoll(std::vector<int>);
-        int         fdIsServerSocket(int);
-        void        dispatchEvents();
-        void        handlePollInEvent(int fd);
-        void        handlePollOutEvent(int fd);
-        void        handlePollErrorOrHangup(int fd);
-        void        handleCgiOutputPipeError(int fd, std::vector<CGITracker>::iterator& cgiIt); // New
-        void        handleCgiInputPipeError(int fd, std::vector<CGITracker>::iterator& cgiIt);  // New
-        void        handleClientError(int fd);                                                  // New
-        void        createNewClient(int);
-        int         readData(int, std::string&, int&);
-        void        handleReadEvents(int);
-        void        handleClientDisconnect(int fd, int bytesRecv);                              // New
-        void        processRequest(int fd, int contentLength);                        // New
-        void        setupCGI(int fd, std::vector<struct ClientTracker>::iterator& clientIt);    // New
-        void        handleCgiOutput(int fd, std::vector<CGITracker>::iterator& cgiIt);          // New
-        void        handleWritingEvents(int);
-        void        writeCGIInput(int fd, std::vector<CGITracker>::iterator& cgiIt);            // New
-        void        sendClientResponse(int fd, std::vector<struct ClientTracker>::iterator& iterClient); // New
-        ClientRequest ParsingRequest(std::string, int);
-        void        closeSockets();
-        int         searchPage(std::string path);
-        int         isCGI(std::string);
-        std::string prepareResponse(ClientRequest);
-        std::vector<struct ClientTracker>::iterator retrieveClient(int fd);
-        std::vector<struct CGITracker>::iterator retrieveCGI(int fd);
+private:
 
-    private:
-        InfoServer *_serverInfo; // could we perhaps call the struct also serverInfo? Just  because if it has the same name as the class and it can be confusing. 
-        std::vector<struct pollfd> poll_sets;
-        std::vector<struct pollfd>::iterator it;
-        std::vector<struct pollfd>::iterator end;
-        int totBytes;
-        std::string full_buffer;
-        std::vector<int> serverFds;
-        std::vector<struct ClientTracker> clientsQueue;
-        std::vector<struct CGITracker> cgiQueue;
+    InfoServer *_serverInfo;
+    std::vector<struct pollfd> poll_sets;
+    std::vector<struct pollfd>::iterator it;
+	std::vector<struct pollfd>::iterator end;
+    int totBytes;
+    std::string full_buffer;
+    std::vector<int>    serverFds;
+    std::vector<struct client> clientsQueue;
+
 };
-
 #endif
